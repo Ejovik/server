512f486e584f58f6076b02f4460ae577b8b034f1
Ejovik <smirnovivan316@gmail.com>
2025-02-27 21:01:19 +0700

    Изменены POST и GET - теперь могут взаимодействовать и DB

--- main/main.go
+++ main/main.go
@@ -2,31 +2,43 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-var task string
-
 type requestBody struct {
-	Message string `json:"message"`
+	Task   string `json:"task"`
+	IsDone bool   `json:"is_done"`
 }
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %s", task)
+	var tasks []Task
+	DB.Find(&tasks)
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func UpdateTaskHandler(w http.ResponseWriter, r *http.Request) {
 	var reqBody requestBody
 	json.NewDecoder(r.Body).Decode(&reqBody)
-	task = reqBody.Message
+
+	newTask := Task{
+		Task:   reqBody.Task,
+		IsDone: reqBody.IsDone,
+	}
+
+	DB.Create(&newTask)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
 }
 
 func main() {
+	InitDB()
+
+	DB.AutoMigrate(&Task{})
+
 	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
-	router.HandleFunc("/api/task", UpdateTaskHandler).Methods("POST")
+	router.HandleFunc("/api/tasks", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/tasks", UpdateTaskHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
 }
