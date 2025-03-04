f5b3800f443723fa6efdc5a26cc3f92573207f2f
Ejovik <smirnovivan316@gmail.com>
2025-03-04 18:23:06 +0700

    улучшена структура кода

---
+++ cmd/app/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"project/internal/database"
+	"project/internal/handlers"
+	"project/internal/taskService"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	database.InitDB()
+	database.DB.AutoMigrate(&taskService.Task{})
+
+	repo := taskService.NewTaskRepository(database.DB)
+	service := taskService.NewService(repo)
+
+	handler := handlers.NewHandler(service)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/api/tasks", handler.GetTasksHandler).Methods("GET")
+	router.HandleFunc("/api/tasks", handler.PostTaskHandler).Methods("POST")
+	router.HandleFunc("/api/tasks/{id}", handler.PatchTaskHandler).Methods("PATCH")
+	router.HandleFunc("/api/tasks/{id}", handler.DeleteTaskHandler).Methods("DELETE")
+	http.ListenAndServe(":8080", router)
+}
